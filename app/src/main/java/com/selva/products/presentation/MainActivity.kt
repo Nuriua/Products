d344f2eeec579585af0acf8cccbf199bae72e3bd
Nuriya Akhmedshina <nyriya36@gmail.com>
2024-05-03 09:22:24 +0500

    Initial commit

---
+++ app/src/main/java/com/selva/products/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.selva.products.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.selva.products.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
